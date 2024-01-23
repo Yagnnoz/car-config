b25822e9d5543baa176b5410ea757162aaef6e52
Yagnnoz <jensk37@gmail.com>
2024-01-23 23:25:04 +0100

    ??

--- src/app/components/model-select/model-select.component.ts
+++ src/app/components/model-select/model-select.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-model-select',
   standalone: true,
   imports: [],
   templateUrl: './model-select.component.html',
-  styleUrl: './model-select.component.scss'
+  styleUrl: './model-select.component.scss',
 })
-export class ModelSelectComponent {
+export class ModelSelectComponent implements OnInit, OnDestroy {
 
+  models?: Subscription;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.models = this.http.get('/models').subscribe(console.log);
+
+  }
+
+  ngOnDestroy() {
+    this.models?.unsubscribe();
+  }
 }
